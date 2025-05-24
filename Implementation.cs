19679c601153b78d726a29f756e45ae006dfc815
Marcy <87911841+DemonBunnyBon@users.noreply.github.com>
2025-05-24 10:51:28 +0200

    Add files via upload

---
+++ Implementation.cs
@@ -0,0 +1,28 @@
+﻿using MelonLoader;
+using UnityEngine;
+using Il2CppInterop;
+using Il2CppInterop.Runtime.Injection; 
+using System.Collections;
+using Il2CppTLD.Gear;
+using Il2Cpp;
+using Il2CppTLD.IntBackedUnit;
+using Il2CppVLB;
+using Il2CppSystem;
+using Il2CppSWS;
+using Harmony;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+namespace InfiniteCheatDeath
+{
+	public class InfiniteCheatDeathMelon : MelonMod
+	{
+        public override void OnInitializeMelon()
+		{
+            MelonLogger.Msg("I always come back...");
+        }
+
+
+    }
+}
---
+++ Patches.cs
@@ -0,0 +1,39 @@
+﻿using MelonLoader;
+using UnityEngine;
+using Il2CppInterop;
+using Il2CppInterop.Runtime.Injection;
+using System.Collections;
+using Il2CppTLD.Gear;
+using Il2Cpp;
+using Il2CppTLD.IntBackedUnit;
+using Il2CppVLB;
+using Il2CppNodeCanvas.Tasks.Actions;
+using Il2CppSteamworks;
+using Il2CppTLD.UI;
+using Il2CppInterop.Runtime.InteropTypes.Arrays;
+using UnityEngine.UIElements;
+using Il2CppTLD.Gameplay;
+
+
+namespace InfiniteCheatDeath
+{
+
+    internal static class Patches
+    {
+        [HarmonyPatch(typeof(LifeAfterDeathManager), "PerformRespawn")]
+
+        public class LifeAfterDeathCountPatch
+        {
+            public static void Postfix(ref LifeAfterDeathManager __instance)
+            {
+
+               __instance.m_LifeAfterDeathState.m_LivesUsed = 0;
+
+            }
+
+        }
+
+
+    }
+}
+
